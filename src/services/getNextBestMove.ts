60706d390c53fb6d80056adf728a13f8d56d8b6e
Joel Field <joelmatthewfield@outlook.com>
2021-12-25 11:39:24 +1000

    handle chess api being down

--- src/services/getNextBestMove.ts
+++ src/services/getNextBestMove.ts
@@ -1,5 +1,8 @@
-export const getNextBestMove = (fen: string): Promise<string> => {
-  return new Promise(async (resolve) => {
+import { ChessInstance } from "chess.js";
+const Chess = require("chess.js");
+
+export const getNextBestMove = async (fen: string): Promise<string> => {
+  try {
     const res = await fetch("http://localhost:3001/bestMove", {
       method: "POST",
       body: JSON.stringify({
@@ -13,6 +16,16 @@ export const getNextBestMove = (fen: string): Promise<string> => {
       .json()
       .then((payload: { bestmove: string; ponder?: string }) => payload);
 
-    return resolve(prediction.bestmove);
-  });
+    return prediction.bestmove;
+  } catch {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        // do a random valid move
+        const chessClient: ChessInstance = Chess(fen);
+        const moves = chessClient.moves();
+        const move = moves[Math.floor(Math.random() * moves.length)];
+        return resolve(move);
+      }, 800);
+    });
+  }
 };
